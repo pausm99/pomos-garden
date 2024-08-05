e804776e9a02dba6f0415d70a74b01f4e4799316
Frank Mullerat <frankmullerat@gmail.com>
2024-08-05 12:27:40 +0200

    Added CRUD operations for Tag Model.

---
+++ actions/tags.ts
@@ -0,0 +1,26 @@
+import { serverCreateTag, serverUpdateTag, serverDeleteTag, serverGetAllTagsForUser } from "@/lib/tags";
+
+// Creating the CRUD actions for tags
+
+//Create Tag Function
+export async function actionCreateTag(userId: string, tagDesc: string, color: string, taskId: string[]) {
+    return await serverCreateTag(userId, tagDesc, color, taskId);
+}
+
+//Get All Tags Function
+
+export async function actionGetAllTagsForUser(userId: string) {
+    return await serverGetAllTagsForUser(userId);
+}
+
+//Update Tag Function
+
+export async function actionUpdateTag(tagId: string, tagDesc?: string, color?: string, taskId?: string[]) {
+    return await serverUpdateTag(tagId, tagDesc, color, taskId);
+}
+
+//Delete Tag Function
+
+export async function actionDeleteTag(tagId: string) {
+    return await serverDeleteTag(tagId);
+}
