f55fbded4aaf0b2003571950107164a51fb041ca
harmutcoral <coral.harmut98@gmail.com>
2024-08-29 16:53:01 +0200

    dnd working in the same column

--- components/TasksSection.tsx
+++ components/TasksSection.tsx
@@ -4,6 +4,7 @@ import { useTasksContext } from "@/contexts/TasksContext";
 import { useEffect, useState } from "react";
 import TodoState from "./TodoState";
 import Loading from "./atoms/Loading";
+import { Task as TaskType } from "@/prisma/generated/zod";
 
 export default function TaskSection() {
   const { tasksCollection, loading } = useTasksContext();
@@ -22,20 +23,25 @@ export default function TaskSection() {
     });
   }, [tasksCollection]);
 
+  const handleTasksChange = (
+    state: "todo" | "doing" | "done",
+    newTasks: TaskType[]
+  ) => {
+    setTasks((prevTasks) => ({ ...prevTasks, [state]: newTasks }));
+  };
+
   return (
     <>
       <Loading isOpen={loading} />
       <div className="p-5 h-full flex gap-5 overflow-hidden">
         {(["todo", "doing", "done"] as Array<keyof typeof tasks>).map(
           (state) => (
-            <div
-              key={state}
-              className="flex-1 w-[33%] flex flex-col gap-2.5"
-            >
+            <div key={state} className="flex-1 w-[33%] flex flex-col gap-2.5">
               <TodoState
                 state={state}
                 name={state.charAt(0).toUpperCase() + state.slice(1)}
                 tasks={tasks[state]}
+                onTasksChange={handleTasksChange}
               />
             </div>
           )
