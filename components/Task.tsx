b0720158c54bfdd4f10f270908e6bda6d90a812b
harmutcoral <coral.harmut98@gmail.com>
2024-09-08 16:13:08 +0200

    sortable same column

--- components/Task.tsx
+++ components/Task.tsx
@@ -12,6 +12,7 @@ import { useTagsContext } from "@/contexts/TagsContext";
 import Tag from "./atoms/Tag";
 import { TaskStatus } from "@/interfaces/State";
 import { SelectState } from "./atoms/SelectState";
+import { useSortable } from "@dnd-kit/sortable";
 
 type TaskProps = {
   task: TaskType;
@@ -26,7 +27,12 @@ export default function Task({ task }: TaskProps) {
   );
   const [isEditing, setIsEditing] = useState(false);
 
+  // DnD kit sortable hook
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: task.id });
+
   const style = {
+    transition,
     boxShadow: "0px 1px 3px rgba(0,0,0,0.1)",
   };
 
@@ -77,6 +83,7 @@ export default function Task({ task }: TaskProps) {
 
   return (
     <div
+      ref={setNodeRef}
       style={style}
       className="flex flex-col gap-5 p-4 bg-white text-black rounded-lg"
     >
@@ -89,7 +96,11 @@ export default function Task({ task }: TaskProps) {
               onChange={handleTitleChange}
             />
           ) : (
-            <h3 className="flex-1 text-zinc-700 text-ellipsis whitespace-nowrap overflow-hidden">
+            <h3
+              className="flex-1 text-zinc-700 text-ellipsis whitespace-nowrap overflow-hidden"
+              {...listeners} // Only make the title draggable
+              {...attributes}
+            >
               {titleInputText}
             </h3>
           )}
--- components/TasksSection.tsx
+++ components/TasksSection.tsx
@@ -1,9 +1,14 @@
-"use client";
-
+// TaskSection.tsx
 import { useTasksContext } from "@/contexts/TasksContext";
 import { useEffect, useState } from "react";
 import TodoState from "./TodoState";
 import Loading from "./atoms/Loading";
+import { DndContext, closestCenter } from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 import { Task as TaskType } from "@/prisma/generated/zod";
 
 export default function TaskSection() {
@@ -30,23 +35,47 @@ export default function TaskSection() {
     setTasks((prevTasks) => ({ ...prevTasks, [state]: newTasks }));
   };
 
+  const onDragEnd = (event: any) => {
+    const { active, over } = event;
+
+    if (!over) return;
+
+    if (active.id !== over.id) {
+      const state = Object.keys(tasks).find((state) =>
+        tasks[state as keyof typeof tasks].find((task) => task.id === active.id)
+      ) as keyof typeof tasks;
+
+      const oldIndex = tasks[state].findIndex((task) => task.id === active.id);
+      const newIndex = tasks[state].findIndex((task) => task.id === over.id);
+
+      handleTasksChange(state, arrayMove(tasks[state], oldIndex, newIndex));
+    }
+  };
+
   return (
     <>
       <Loading isOpen={loading} />
-      <div className="p-5 h-full flex gap-5 overflow-hidden">
-        {(["todo", "doing", "done"] as Array<keyof typeof tasks>).map(
-          (state) => (
-            <div key={state} className="flex-1 w-[33%] flex flex-col gap-2.5">
-              <TodoState
-                state={state}
-                name={state.charAt(0).toUpperCase() + state.slice(1)}
-                tasks={tasks[state]}
-                onTasksChange={handleTasksChange}
-              />
-            </div>
-          )
-        )}
-      </div>
+      <DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd}>
+        <div className="p-5 h-full flex gap-5 overflow-hidden">
+          {(["todo", "doing", "done"] as Array<keyof typeof tasks>).map(
+            (state) => (
+              <div key={state} className="flex-1 w-[33%] flex flex-col gap-2.5">
+                <SortableContext
+                  items={tasks[state].map((task) => task.id)}
+                  strategy={verticalListSortingStrategy}
+                >
+                  <TodoState
+                    state={state}
+                    name={state.charAt(0).toUpperCase() + state.slice(1)}
+                    tasks={tasks[state]}
+                    onTasksChange={handleTasksChange}
+                  />
+                </SortableContext>
+              </div>
+            )
+          )}
+        </div>
+      </DndContext>
     </>
   );
 }
