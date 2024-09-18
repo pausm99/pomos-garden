c09bf312c617592a7e78a93ba88a0f72fd4558fb
pausm99 <pausm99@gmail.com>
2024-09-18 17:14:48 +0200

    fix eslint warning

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -1,9 +1,10 @@
 "use client";
+
 import Section from "@/components/Section";
 import { useUserContext } from "@/contexts/UserContext";
 import React from "react";
 
-export default function page() {
+export default function Page() {
   const { user } = useUserContext()
   return <Section section="stats" name={`Analytics Dashboard for ${user?.name || '...'}`} />;
 }
