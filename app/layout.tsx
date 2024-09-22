fbc45007bb36c0256a97e51512d865a176a32c94
aliem-r <ali.e.m@outlook.com>
2024-09-22 13:35:22 +0200

    Add fixed height to main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,9 +39,9 @@ export default function RootLayout({
             <TimerProvider>
               <div className="flex flex-col min-h-screen h-screen px-[1em] max-w-[1400px] mx-auto py-5">
                 <Header />
-                <div className="h-full flex gap-7 overflow-hidden">
+                <div className="h-full flex gap-7">
                   <ServicesNav className="w-20 flex flex-col gap-5" />
-                  <div className="h-full w-full flex rounded-4xl bg-zinc-100 overflow-y-auto overflow-x-auto relative">
+                  <div className="h-[733px] w-full flex rounded-4xl bg-zinc-100 overflow-y-auto overflow-x-auto relative">
                     <Blob className="absolute w-full h-full max-w-full max-h-full overflow-hidden" />
                     <div className="absolute w-full h-full backdrop-blur-3xl"></div>
                     <div className="flex-1 z-10 no-cursor">{children}</div>
